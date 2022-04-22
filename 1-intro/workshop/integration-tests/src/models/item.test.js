cb73544c0002626705c0596b6b49809415f1fd09
LeoneLal <leone.lalloue@hotmail.fr>
2022-04-22 14:46:48 +0200

    Add create Item tests

---
+++ 1-intro/workshop/integration-tests/src/models/item.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Item = require('./Item.js');
+
+
+beforeAll(async () => {
+    await mongoose.connect('mongodb://mongo:27017/docker-node-mongo-test', { useNewUrlParser: true });
+    return Item.deleteMany({})
+});
+
+describe('Create items', () => {
+    
+    it('should be created and saved', async () => {
+        Item.deleteMany({})
+        const item = new Item({ name: 'Théo' });
+        const savedItem = await item.save();
+        const findItem = await Item.findOne({ name: "Théo" });
+        expect(findItem.name).toBe(item.name);
+    });
+
+    // it("shouldn't be created and saved", async () => {
+    //     Item.deleteMany({});
+    //     const item = new Item({});
+    //     expect(item.save()).rejects.toThrow("The item needs a name");
+    // });
+
+    // it("should change the type of name", async () => {
+    //     Item.deleteMany({});
+    //     // const item = new Item({name: [123]});
+    //     expect(new Item({name: [123]}).save()).rejects.toThrow();
+    //     // expect(item.save()).rejects.toThrow("The item needs a type string name");
+    //     // expect(typeof item.name).toBe("string")
+    // });
+});
