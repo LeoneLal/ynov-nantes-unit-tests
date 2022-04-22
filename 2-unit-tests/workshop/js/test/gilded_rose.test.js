68734edb6ad5518c373cc78df6f0577ea7a946a8
Auriane Labille <auriane.labille@gmail.com>
2022-04-22 16:17:36 +0200

    Update gilded_rose.test.js

--- 2-unit-tests/workshop/js/test/gilded_rose.test.js
+++ 2-unit-tests/workshop/js/test/gilded_rose.test.js
@@ -1,15 +1,24 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", function () {
+  it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("fixme");
+  });
+
   // Tous les éléments ont une valeur sellIn qui désigne le nombre de jours restant pour vendre l'article.
   it("should be equal to number of days left to sell the item", function () {
     const gildedRose = new Shop([new Item("foo", 10, 10)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(9);
   });
+
+  // À la fin de chaque journée, notre système diminue ces deux valeurs pour chaque produit.  
+  it("should decrement", function () {
+    const gildedRose = new Shop([new Item("foo", 10, 10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(9);
   });
 });
