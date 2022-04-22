e3449a6a64092076dd385eb968c278965e15ad2f
Yessi Munguengui <yessimunguengui@gmail.com>
2022-04-22 16:34:10 +0200

    fix : add product quality unit test

--- 2-unit-tests/workshop/js/test/gilded_rose.test.js
+++ 2-unit-tests/workshop/js/test/gilded_rose.test.js
@@ -1,8 +1,10 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Names", function() {
-  it("shouldn't change", function() {
-    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros",100, 80)]);
+describe("Names", function () {
+  it("shouldn't change", function () {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 100, 80),
+    ]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(80);
   });
@@ -13,44 +15,51 @@ describe("Names", function() {
   //   expect(items[0].quality).toBe(13);
   // })
 
-  it("should get better", function() {
-    const Backstage = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]);
-    const Backstage2 = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)]);
-    const Backstage3 = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)]);
-    const BackstageOld = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", -2, 20)]);
-    
-    let items = Backstage.updateQuality();    
-    expect(items[0].quality).toBe(21);
-    items = Backstage2.updateQuality();    
-    expect(items[0].quality).toBe(22);
-    items = Backstage3.updateQuality();    
-    expect(items[0].quality).toBe(23);
-    items = BackstageOld.updateQuality();    
-    expect(items[0].quality).toBe(0);
-  })
+  it("should get better", function () {
+    const Backstage = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    ]);
+    const Backstage2 = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+    ]);
+    const Backstage3 = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+    ]);
+    const BackstageOld = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", -2, 20),
+    ]);
 
-  it("should get better", function() {
+    let items = Backstage.updateQuality();
+    expect(items[0].quality).toBe(21);
+    items = Backstage2.updateQuality();
+    expect(items[0].quality).toBe(22);
+    items = Backstage3.updateQuality();
+    expect(items[0].quality).toBe(23);
+    items = BackstageOld.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("should get better", function () {
     const Brie = new Shop([new Item("Aged Brie", 15, 20)]);
     const Brie4 = new Shop([new Item("Aged Brie", -5, 5)]);
-    
-    let items = Brie.updateQuality();    
+
+    let items = Brie.updateQuality();
     expect(items[0].quality).toBe(21);
 
-    items = Brie4.updateQuality();    
+    items = Brie4.updateQuality();
     expect(items[0].quality).toBe(7);
-  })
-  
-  it("should get worst", function() {
+  });
+
+  it("should get worst", function () {
     const rndObject = new Shop([new Item("Random object", 15, 20)]);
     const rndObject2 = new Shop([new Item("Random object", -5, 5)]);
-    
-    let items = rndObject.updateQuality();    
+
+    let items = rndObject.updateQuality();
     expect(items[0].quality).toBe(19);
 
-    items = rndObject2.updateQuality();    
+    items = rndObject2.updateQuality();
     expect(items[0].quality).toBe(3);
-  })
-  
+  });
 
   // Tous les éléments ont une valeur sellIn qui désigne le nombre de jours restant pour vendre l'article.
   it("should be equal to number of days left to sell the item", function () {
@@ -59,7 +68,7 @@ describe("Names", function() {
     expect(items[0].sellIn).toBe(9);
   });
 
-  // À la fin de chaque journée, notre système diminue ces deux valeurs pour chaque produit.  
+  // À la fin de chaque journée, notre système diminue ces deux valeurs pour chaque produit.
   it("should decrement", function () {
     const gildedRose = new Shop([new Item("foo", 10, 10)]);
     const items = gildedRose.updateQuality();
@@ -67,11 +76,31 @@ describe("Names", function() {
     expect(items[0].quality).toBe(9);
   });
 
-  // Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.  
+  // Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.
   it("should decrement quality twice fast", function () {
     const gildedRose = new Shop([new Item("foo", 0, 10)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(-1);
     expect(items[0].quality).toBe(8);
+  });
+});
+
+describe("Product quality", function () {
+  it("should be under 50", function () {
+    const gildedRose = new Shop([new Item("bottle", 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBeLessThan(50);
+  });
+
+  it("should be positive", function () {
+    const gildedRose = new Shop([new Item("candy", 0, 12)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBeGreaterThanOrEqual(0);
+  });
+
+  it("should be an integer", function () {
+    const gildedRose = new Shop([new Item("candy", 0, 25)]);
+    const items = gildedRose.updateQuality();
+    expect(typeof items[0].quality).toBe("number");
   });
 });
