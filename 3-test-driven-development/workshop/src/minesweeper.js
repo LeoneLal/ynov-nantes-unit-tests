48769e40129d71f1c522f9589cec6b55d6ead5fe
Yessi Munguengui <yessimunguengui@gmail.com>
2022-04-28 11:29:18 +0200

    feat: test bombs quantity

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -1,15 +1,20 @@
-class Minesweeper{
-    createField(columns,lines){
-        let field = "";
-        for(let i = 0; i<columns; i++){
-            field += ".".repeat(lines) 
-            field += "\n";
-        }
-       console.log(field)
-        return field;
+class Minesweeper {
+  createField(columns, lines) {
+    let field = "";
+    for (let i = 0; i < columns; i++) {
+      field += ".".repeat(lines);
+      field += "\n";
     }
+    console.log(field);
+    return field;
+  }
 
+  bombsQuantity(columns, lines) {
+    let size = columns * lines;
+    let ratio = 2;
+    let standardSize = 16;
+    let quantity = Math.round((size * ratio) / standardSize);
+    return quantity;
+  }
 }
-
-module.exports = { Minesweeper }
-  
+module.exports = { Minesweeper };
--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,10 +1,17 @@
 const { Minesweeper } = require("../src/minesweeper");
 
-describe("Gilded Rose", function () {
-    it("should return true", function () {
-        const minesweeper = new Minesweeper([]);
-        const items = minesweeper.createField(3,3);
-        expect(items).toBe("...\n...\n...\n");
-      });
-    
-})
+describe("minesweeper", function () {
+  it("should return true", function () {
+    const minesweeper = new Minesweeper([]);
+    const items = minesweeper.createField(3, 3);
+    expect(items).toBe("...\n...\n...\n");
+  });
+});
+
+describe("bombs quantity", function () {
+  it("should return the good ratio", function () {
+    const minesweeper = new Minesweeper([]);
+    const bombsQuantity = minesweeper.bombsQuantity(6, 5);
+    expect(bombsQuantity).toBe(4);
+  });
+});
